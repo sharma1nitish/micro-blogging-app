5b286f86b2b22537ae5d1e0c2ee4ee242b050e70
nitish <sharma1nitish@gmail.com>
2014-07-12 05:08:59 +0530

    Finished user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+	def gravatar_for(user) 
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=http%3A%2F%2Fimg839.imageshack.us%2Fimg839%2F7595%2Ftargaryen3grat.png"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end	
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Nitish Sharma"
+		email "sharma1nitish@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end	
+end
